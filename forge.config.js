16a8c01799cbef25f77053b24f9b8ad80cb83bb6
王会 <wanghuixjgc@163.com>
2025-10-02 21:50:00 +0800

    苹果打印修改

--- forge.config.js
+++ forge.config.js
@@ -4,34 +4,59 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: 'assets/icon',
+    /*name: 'ponygorx-order-printer',
+    executableName: 'ponygorx-order-printer',
+    icon: 'assets/icons', // 通用图标路径（无扩展名）
+    // 可以指定特定平台的图标
+    win32metadata: {
+      'requested-execution-level': 'asInvoker',
+      'file-description': 'ponygorx order printer',
+      'product-version': '1.0.2',
+      'company-name': 'PonyGo',
+      'original-filename': 'ponygorx-order-printer.exe',
+      'internal-name': 'ponygorx-order-printer'
+    }*/
   },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin','win32'],
-    },
-    {
-      name: '@electron-forge/maker-dmg',
+      name: '@electron-forge/maker-squirrel',
       config: {
-       /* background: './assets/dmg-background.png',
-        format: 'ULFO'*/
+        iconUrl: 'https://www.insdoor.com/favicon.ico',
+        setupIcon: 'assets/icon.ico', // 指定安装程序的图标路径
+       /* name: 'ponygorx-order-printer',
+        authors: 'ponygo',
+        exe: 'ponygorx-order-printer.exe',
+        description: 'ponygorx order printer',
+        version: '1.0.2',
+        loadingGif: '', // 可选：安装时的加载动画
+        setupExe: 'ponygorx-order-printer-1.0.2.exe',
+        noMsi: true,
+        // 创建快捷方式设置
+        createDesktopShortcut: true,
+        createStartMenuShortcut: true,
+        // 手动设置桌面快捷方式图标
+        shortcutFolderName: 'ponygorx-order-printer',
+        shortcutName: 'ponygorx-order-printer',*/
       }
     },
 
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
 
     {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
+      name: '@electron-forge/maker-zip',
+      config: {
+        iconUrl: 'https://www.insdoor.com/favicon.ico',
+        setupIcon: 'assets/icon.ico', // 指定安装程序的图标路径
+        }
+    }
+    /*,
     {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+      name: '@electron-forge/maker-wix',
+      config: {
+        icon: 'assets/icon.ico',
+      }
+    }*/
   ],
   plugins: [
     {
--- main.js
+++ main.js
@@ -1,34 +1,36 @@
 const { app, BrowserWindow,Menu, ipcMain, ipcRenderer} = require('electron');
 const path = require('path');
 const { exec } = require('child_process');
-const { print } = require('pdf-to-printer');
 const fs = require('fs');
 const https = require('https');
 const http = require('http');
-
-
 const iconv = require('iconv-lite'); // 需要先安装iconv-lite
-
 // 处理Windows平台的快捷方式
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
-// 设置 SumatraPDF 的路径
-//process.env.SUMATRA_PDF_PATH = 'D:\\order-printer\\resources\\pdf\\SumatraPDF-3.4.6-32.exe';
-
 // 设置 Node.js 默认字符编码为 UTF-8
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 process.env.LANG = 'zh_CN.UTF-8';
-
 let mainWindow;
+let printSize="small";
 
-
+/*app.setUserTasks([
+  {
+    program: process.execPath,
+    arguments: '--new-window',
+    iconPath: process.execPath,
+    iconIndex: 0,
+    title: 'New Window',
+    description: 'Create a new window'
+  }
+]);*/
 function createWindow() {
-
   mainWindow = new BrowserWindow({
     width: 800,
     height: 400,
-    icon: path.join(__dirname, 'assets', 'icon.ico'), // 添加窗口图标
+   // icon: '/path/to/icon.png'
+    icon: path.join(__dirname, 'assets', 'icon.png'), // 添加窗口图标
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -68,60 +70,42 @@ app.whenReady().then(() => {
 ipcMain.on('get-printers', (event) => {
   console.log('Getting printers...获取打印机=====');
   let command;
-  if (process.platform === 'win32') {
-    // Windows 平台
+  if (process.platform === 'win32') {// Windows 平台
     command = 'powershell -NoProfile -Command "$OutputEncoding = [System.Text.Encoding]::UTF8; Get-Printer | Select-Object -ExpandProperty Name"';
-  } else if (process.platform === 'darwin') {
-    // macOS 平台
-    command = 'system_profiler SPPrintersDataType -xml | grep -A 1 "_name" | grep string | sed "s/.*<string>\\(.*\\)<\\/string>.*/\\1/"';
-  } else {
-    // Linux 平台
-    command = 'lpstat -p';
-  }
-  exec(command, {encoding: 'buffer' }, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`执行命令出错: ${error}`);
-      event.reply('printers-list', []);
-      return;
-    }
-
-    let printers = [];
-    if (process.platform === 'win32') {
-
+    exec(command, {encoding: 'buffer' }, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`执行命令出错: ${error}`);
+        event.reply('printers-list', []);
+        return;
+      }
+      let printers = [];
       // 使用iconv-lite将输出从GBK（中文系统默认）转换为UTF-8
       const decodedOutput = iconv.decode(stdout, 'gbk');
       // 解析 PowerShell 命令输出
       const lines = decodedOutput.trim().split('\n');
-    //  console.log('Windows 打印机列表:', lines); // 确保这里输出的是正确的编码
-
       for (const line of lines) {
         const printerName = line.trim();
         if (printerName) {
           printers.push({ name: printerName });
         }
       }
-    } else {
-     /* // 解析 Linux/macOS 命令输出
-      const lines = stdout.trim().split('\n');
-      for (const line of lines) {
-        const match = line.match(/printer (.+?) is/);
-        if (match) {
-          printers.push({ name: match[1] });
+      event.reply('printers-list', printers);
+    });
+  } else if (process.platform === 'darwin') { // macOS 平台
+      const { getPrinters} = require('unix-print');
+      getPrinters().then((res)=>
+        (printers) => {
+          console.log('macOS 打印机列表:', printers);
+          event.reply('printers-list', printers);
         }
-      }*/
-          // 将Buffer转换为字符串
-          const output = stdout.toString();  // 添加这行来转换Buffer到字符串
-          // 解析 Linux/macOS 命令输出
-          const lines = output.trim().split('\n');
-          for (const line of lines) {
-            const printerName = line.trim();
-            if (printerName) {
-              printers.push({ name: printerName });
-            }
-          }
-    }
-    event.reply('printers-list', printers);
-  });
+      );
+  //  command = 'system_profiler SPPrintersDataType -xml | grep -A 1 "_name" | grep string | sed "s/.*<string>\\(.*\\)<\\/string>.*/\\1/"';
+  } /*else {
+    // Linux 平台
+    command = 'lpstat -p';
+  }*/
+
+
 });
 // 下载网络PDF文件
 async function downloadPDF(url) {
@@ -204,102 +188,71 @@ function getSumatraPath() {
   return sumatraPath;
 }
 
-async function printNetworkPDF(pdfUrl, options = {}) {
-  console.log("printNetworkPDF===pdfUrl:", pdfUrl);
-  // 下载PDF到临时文件
-  const tempFilePath = await downloadPDF(pdfUrl);
-  const sumatraPath = getSumatraPath();
-  // 设置打印选项
-  const printOptions = {
-    printer: options.printer || '',
-    paperSize: {
-      width: 102, // 单位：毫米
-      height: 51 // 单位：毫米
-    },
-    scale:'noscale',
-    orientation: 'landscape',
-    sumatraPdfPath: sumatraPath ///如果是苹果电脑还需要这个吗？？？？
-  }
-  console.log("临时文件",tempFilePath);
-  print(tempFilePath,printOptions).then(
-      (res)=>{
-        console.log("打印结果：",res);
-
-        ///发送打印结果
-        // 打印完成后删除临时文件（延迟删除确保打印完成）
-        setTimeout(() => {
-          if (fs.existsSync(tempFilePath)) {
-            fs.unlinkSync(tempFilePath)
-            console.log('临时文件已删除')
-          }
-        }, 10000) // 10秒后删除
-
-      }
-  ).catch((error) => {
-    console.error('打印错误:', error);
-  });
-}
 
 // 打印网络PDF
 ipcMain.on('print-network-pdf', async (event, pdfUrl, options = {}) => {
   try {
     // 下载PDF到临时文件
     const tempFilePath = await downloadPDF(pdfUrl);
-    // 设置打印选项
-    const printOptions = {
-      printer: options.printer || '',
-      paperSize: {
-        width: 102, // 单位：毫米
-        height: 51 // 单位：毫米
-      },
-      scale:'noscale',
-      orientation: 'landscape'
-     // sumatraPdfPath: sumatraPath
-    }
-    //如果不是苹果系统 则需要添加 sumatraPdfPath
-    if (process.platform !== 'darwin') {
-      const sumatraPath = getSumatraPath();
-      printOptions.sumatraPdfPath = sumatraPath;
-    }
-
-
-
-    console.log("临时文件",tempFilePath);
-    print(tempFilePath,printOptions).then(
-        (res)=>{
-          console.log("打印结果：",res);
-          event.reply('print-result', { success: true, message: '打印成功' });
-          ///发送打印结果
-          // 打印完成后删除临时文件（延迟删除确保打印完成）
-          setTimeout(() => {
-            if (fs.existsSync(tempFilePath)) {
-              fs.unlinkSync(tempFilePath)
-              console.log('临时文件已删除')
-            }
-          }, 10000) // 10秒后删除
+    if(process.platform === 'win32'){ // windows 平台
+        const { print } = require('pdf-to-printer');
+        // 设置打印选项
+        const sumatraPath = getSumatraPath();
+        const printOptions = {
+          printer: options.printer || '',
+          paperSize: {
+            width: 102, // 单位：毫米
+            height: 51 // 单位：毫米
+          },
+          scale:'noscale',
+          orientation: 'landscape',
+          sumatraPdfPath: sumatraPath
         }
-    ).catch((error) => {
-      console.error('打印错误:', error);
-      event.reply('print-result', { success: false, message: '打印失败: ' + error.message });
-    });
-
-   // printNetworkPDF(pdfUrl,options);
-
-    // 打印PDF
-   // await pdfToPrinter(printOptions);
-   /* pdfToPrinter.print(printOptions,).then((success) => {
-      if (success) {
-        event.reply('print-result', { success: true, message: '打印成功' });
-      } else {
-        event.reply('print-result', { success: false, message: '打印失败' });
-      }
-    }).catch((error) => {
-      console.error('打印错误:', error);
-      event.reply('print-result', { success: false, message: '打印失败: ' + error.message });
-    });*/
-   // console.log('打印任务已发送')
-
-
+        if( printSize==="big"){
+          printOptions.paperSize = {
+            width:152, // 单位：毫米
+            height: 102 // 单位：毫米
+          };
+        }
+        console.log("临时文件",tempFilePath);
+        print(tempFilePath,printOptions).then(
+            (res)=>{
+              console.log("打印结果：",res); ///发送打印结果
+              event.reply('print-result', { success: true, message: '打印成功' });
+              setTimeout(() => {// 打印完成后删除临时文件（延迟删除确保打印完成）
+                if (fs.existsSync(tempFilePath)) {
+                  fs.unlinkSync(tempFilePath)
+                  console.log('临时文件已删除')
+                }
+              }, 10000) // 10秒后删除
+            }
+        ).catch((error) => {
+          console.error('打印错误:', error);
+          event.reply('print-result', { success: false, message: '打印失败: ' + error });
+        });
+    }else{
+        let optionsMacOS = ["-o landscape",  "-o media=Custom.4x2in"];
+        if( printSize==="big"){
+           optionsMacOS = ["-o landscape",  "-o media=Custom.4x6in"];
+        }
+        const { print,isPrintComplete} = require('unix-print');
+        try {
+          const printJob = await print(tempFilePath, options.printer, optionsMacOS);
+          console.log('Print job started:', printJob);
+          async function waitForPrintCompletion(printJob) {
+            while (!(await isPrintComplete(printJob))) {
+              // Wait a bit before checking again (to avoid constant checks)
+              await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second
+            }
+            event.reply('print-result', { success: true, message: '打印成功MacOS' });
+            console.log('Job complete');
+          }
+          await waitForPrintCompletion(printJob);
+        } catch (error) {
+          console.error('Error during printing:', error);
+          event.reply('print-result', { success: false, message: '打印失败macos: ' + error.message });
+        }
+    }
   } catch (error) {
  //  console.error('打印网络PDF失败:', error)
    // return { success: false, error: error.message }
@@ -354,7 +307,8 @@ app.on('second-instance', (event, commandLine, workingDirectory) => {
     const parsedUrl = new URL(url);
     const pdfUrl = parsedUrl.searchParams.get('pdfUrl');
     const sn = parsedUrl.searchParams.get('sn');
-    const printerSelect=parsedUrl.searchParams.get('printer');
+    const size=parsedUrl.searchParams.get('size');
+    printSize=size;
    // printNetworkPDF(pdfUrl,{printer:printerSelect});
     // 我们可以存储这个url，等窗口创建后发送
     // 这里我们等待窗口创建后发送
@@ -390,7 +344,8 @@ if (process.platform !== 'darwin') {
       const parsedUrl = new URL(url);
       const pdfUrl = parsedUrl.searchParams.get('pdfUrl');
       const sn = parsedUrl.searchParams.get('sn');
-      const printerSelect=parsedUrl.searchParams.get('printer');
+      const size=parsedUrl.searchParams.get('size');
+      printSize=size;
       console.log("pdfUrl:", pdfUrl);
      // printNetworkPDF(pdfUrl,{printer:printerSelect});
       // 我们可以存储这个url，等窗口创建后发送
@@ -399,7 +354,7 @@ if (process.platform !== 'darwin') {
         if (mainWindow) {
             mainWindow.webContents.once('did-finish-load', () => {
             console.log("protocol-url=======设置打印机的值");
-            mainWindow.webContents.send('protocol-url', sn,pdfUrl);
+            mainWindow.webContents.send('protocol-url',sn,pdfUrl);
           });
         }
       });
